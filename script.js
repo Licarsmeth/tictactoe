b54911cb845f1308810a1d44211f8d205071383f
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-01-17 09:33:33 +0545

    implement basic board, playgame, changeplayer and restart functionalities.

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-const gameboard = function(){
-    const gameArray = [];
-    return{gameArray};
-}();
+const gameboard = (function () {
+  const gameArray = [];
+  return { gameArray };
+})();
 
-const Players = function(name, state=""){
-    return {name, state};
-}
+const Players = function (name, state = "") {
+  return { name, state };
+};
 
-const oPlayer = Players('Player O', 'O');
-const xPlayer = Players('Player X', 'X');
-console.log(oPlayer);
+const oPlayer = Players("O");
+const xPlayer = Players("X");
+// console.log(oPlayer);
+
+//selecting the elements
+const cells = document.querySelectorAll("main div");
+const restart = document.querySelector("button");
+const turn = document.querySelector(".turn");
+
+restart.addEventListener("click", () => {
+  for (let cell of cells) {
+    cell.textContent = "";
+  }
+});
+
+let curr = "X";
+const playGame = () => {
+  //first turn is of X
+  //set the current player variable as curr
+  for (let cell of cells) {
+    cell.addEventListener("click", (e) => {
+      if (e.target.textContent == "") {
+        e.target.textContent = curr;
+        changePlayer();
+      }
+    });
+  }
+};
+
+const changePlayer = () => {
+  //if x then o else x
+  curr = curr == "X" ? "O" : "X";
+  turn.textContent = `${curr}'s Turn!`;
+};
+
+const winCheck = () => {
+  //put current state as 's' and check 's' patters
+  //against the various array possibilities
+};
+playGame();
